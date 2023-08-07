ed138a1fbc20a68746a530a9f4c70c2b7c96a9dd
Biao <biaofu@thoughtworks.com>
2023-08-07 14:45:15 +0800

    add node menu palette

--- packages/minding/src/components/NodeMenuStyle.vue
+++ packages/minding/src/components/NodeMenuStyle.vue
@@ -1,19 +1,25 @@
 <template>
   <v-container>
-    <div class="d-flex justify-start mb-4">
-      <v-btn variant="text" rounded="xs" append-icon="mdi-palette" class="pl-0 text-capitalize">
-        Font color
-        <template v-slot:append>
-          <v-icon color="warning"></v-icon>
-        </template>
-      </v-btn>
+    <div class="d-flex justify-start mt-3 mb-5">
+      <div class="d-flex align-center">
+        <p class="font-weight-light">Font color</p>
+        <v-menu location="end" :close-on-content-click="false">
+          <template v-slot:activator="{ props }">
+            <v-btn v-bind="props" elevation="0" icon="mdi-palette-outline" class="ml-2 palette" :color="fontColor"></v-btn>
+          </template>
+          <v-color-picker v-model="fontColor" elevation="2" class="ma-2" show-swatches swatches-max-height="240px"></v-color-picker>
+        </v-menu>
+      </div>
 
-      <v-btn variant="text" rounded="xs" append-icon="mdi-palette" class="ml-2 text-capitalize">
-        Background color
-        <template v-slot:append>
-          <v-icon color="warning"></v-icon>
-        </template>
-      </v-btn>
+      <div class="d-flex align-center ml-8">
+        <p class="font-weight-light">Background color</p>
+        <v-menu location="end" :close-on-content-click="false">
+          <template v-slot:activator="{ props }">
+            <v-btn v-bind="props" elevation="0" icon="mdi-palette-outline" class="ml-2 palette" :color="backgroundColor"></v-btn>
+          </template>
+          <v-color-picker v-model="backgroundColor" elevation="2" class="ma-2" show-swatches swatches-max-height="240px"></v-color-picker>
+        </v-menu>
+      </div>
     </div>
 
     <v-select clearable label="Font size" :items="fontSizes" variant="outlined"></v-select>
@@ -25,7 +31,6 @@
 
 <script setup>
 import { ref } from 'vue';
-
 const fontSizes = ['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming'];
 const fontWeights = [
   'Black text',
@@ -37,8 +42,18 @@ const fontWeights = [
   'Italic text'
 ];
 const textDecoration = ['none', 'underline', 'overline', 'line-through'];
+
+const fontColor = ref('#777777');
+const backgroundColor = ref('transparent');
+
 </script>
 
 <style>
+.palette {
+  color: #d7d4d4;
+  font-size: x-small;
+  height: 24px !important;
+  width: 24px !important;
+}
 
 </style>
